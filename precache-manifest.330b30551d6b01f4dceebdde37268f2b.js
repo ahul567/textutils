2b527dc7f9e23ada94747ea3cb89f1773117d1b3
ahul567 <rahulrajrollno.25tmp@gmail.com>
2025-03-22 15:29:53 +0530

    Updates

---
+++ precache-manifest.330b30551d6b01f4dceebdde37268f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff53f443ae0a4fef4335d2760bc93c2c",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "8f4b44c089ea86567acb",
+    "url": "/textutils/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/textutils/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/textutils/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "8f4b44c089ea86567acb",
+    "url": "/textutils/static/js/main.52475396.chunk.js"
+  },
+  {
+    "revision": "7a8be6afc65844edb6f3",
+    "url": "/textutils/static/js/runtime~main.0b59052a.js"
+  }
+]);
